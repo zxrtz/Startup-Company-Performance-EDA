a637ed259405b1bb8cffb84f4f33242c95361b32
Dylan N. <dylannguyen.inb@gmail.com>
2025-03-01 21:57:21 -0500

    Add files via upload

---
+++ EDA_startup_data.sql
@@ -0,0 +1,100 @@
+
+-- Startups Data
+
+-- staging step 1
+SELECT *
+FROM startup_data;
+
+DROP TABLE startup_data_staging1;
+
+CREATE TABLE startup_data_staging1
+LIKE startup_data;
+
+INSERT INTO startup_data_staging1
+SELECT * FROM startup_data;
+
+SELECT *
+FROM startup_data_staging1;
+
+
+-- standardize data step 2
+
+ALTER TABLE startup_data_staging1
+RENAME COLUMN `Funding Amount (M USD)` TO funding_amount,
+RENAME COLUMN `Startup Name` TO startup_name,
+RENAME COLUMN `Funding Rounds` TO funding_rounds,
+RENAME COLUMN `Valuation (M USD)` TO valuation,
+RENAME COLUMN `Revenue (M USD)` TO revenue,
+RENAME COLUMN `Market Share (%)` TO market_share_percentage,
+RENAME COLUMN `Year Founded` TO year_founded,
+RENAME COLUMN `Exit Status` TO exit_status;
+
+SELECT *
+FROM startup_data_staging1;
+
+-- exploration phase 3
+SELECT startup_name,industry, valuation, revenue, market_share_percentage, funding_rounds
+FROM startup_data_staging1
+ORDER BY valuation DESC, revenue DESC, market_share_percentage DESC;
+
+
+
+
+
+-- ranks valuation of companies by industry in north america
+-- easy to see that the top startups in valuation that are profitable are e commerce
+-- cybersec, edtech, and healthtech are all not very profitable for the top valuation companies
+WITH industry_rank AS 
+(
+SELECT Industry, startup_name, valuation, Profitable,
+DENSE_RANK() OVER(PARTITION BY industry ORDER BY valuation DESC) AS valuation_industry_rank
+FROM startup_data_staging1
+WHERE Region = 'North America'
+)
+SELECT *
+FROM industry_rank
+WHERE valuation_industry_rank <= 5
+;
+
+
+
+
+
+
+-- calculates averages
+SELECT industry, SUBSTR(AVG(Valuation), 1, 7) avg_valuation, 
+				SUBSTR(AVG(Revenue), 1, 5) avg_revenue, 
+                SUBSTR(AVG(funding_amount), 1, 6) avg_funding,
+                AVG(Profitable) profitability
+FROM startup_data_staging1
+GROUP BY industry
+ORDER BY 2 DESC;
+
+
+SELECT industry, AVG(Profitable) profitability_chance
+FROM startup_data_staging1
+GROUP BY industry;
+
+
+
+
+-- showing the probablity of profitability of startups in america based on industry
+WITH profitability_ranker AS
+(
+SELECT industry, SUBSTR(AVG(Valuation), 1, 7) avg_valuation, 
+				SUBSTR(AVG(Revenue), 1, 5) avg_revenue, 
+                SUBSTR(AVG(funding_amount), 1, 6) avg_funding,
+                AVG(Profitable) profitability_chance,
+				SUBSTR(AVG(valuation)/AVG(funding_amount), 1, 5) valuation_funding_ratio
+FROM startup_data_staging1
+WHERE Region = 'North America'
+GROUP BY industry
+)
+SELECT industry, profitability_chance, avg_funding, avg_valuation, valuation_funding_ratio
+FROM profitability_ranker
+ORDER BY profitability_chance DESC
+;
+
+SELECT *
+FROM startup_data_staging1;
+
